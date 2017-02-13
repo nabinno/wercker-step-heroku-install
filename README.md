54d309f7066c8937085316fa2e9be9661b06508a
Nab Inno <nab@blahfe.com>
2017-02-14 01:12:08 +0900

    Fork `wercker/step-heroku-deploy` for step heroku-install.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# Heroku deployment step
+# Heroku install step
 
-Deploy your code to Heroku. This step requires that you deploy to a Heroku
-deploy target. By default this will create a new ssh-key for each deploy, see
-`Using wercker SSH key pair`.
+Install heroku-toolbelt. By default this will create a new ssh-key for
+each deploy, see `Using wercker SSH key pair`.
 
 # Dependencies
 
@@ -35,26 +34,13 @@ The following dependencies are only required when using temporary keys (see:
   Alternatively you can use `$HEROKU_APP_NAME`.
 * `key-name` (optional) Specify the name of the key that should be used for this
   deployment. If left empty, a temporary key will be created for the deployment.
-* `retry` (optional) When a deploy to Heroku fails, a new deploy is
-  automatically performed after 5 seconds. If you want to disable this behavior,
-  set `retry` to `false`.
-* `run` (optional) Run a command on heroku after the code is deployed
-  succesfully. This option can be used to migrate the database for example.
-* `install-toolbelt` (optional). If set to 'true', the toolbelt will be
-  installed. Note: specifying a run command will also install the toolbelt.
-* `keep-repository` (optional) This will allow a user to keep the original
-  history of the repository, speeding up deployment. **Important:** changes made
-  during the build will not be deployed. Also keep in mind that deploying an
-  already up to date repo will not result in an application restart. Use the
-  `run` parameter to forcibly reload to achieve this. This feature is considered
-  beta, expect issues. If you find one, please contact us.
 
 # Example
 
 ``` yaml
 deploy:
     steps:
-        - heroku-deploy:
+        - nabinno/heroku-install:
             key-name: MY_DEPLOY_KEY
 ```
 
@@ -77,13 +63,13 @@ manually add the public key to Heroku.
       in the last step).
     - Select `SSH Key pair` as the type and select the key pair which you
       created earlier.
-- In the `heroku-deploy` step in your `wercker.yml` add the `key-name` property
-  with the value you used earlier:
+- In the `nabinno/heroku-install` step in your `wercker.yml` add the
+  `key-name` property with the value you used earlier:
 
 ```yaml
 deploy:
     steps:
-        - heroku-deploy:
+        - nabinno/heroku-install:
             key-name: MY_DEPLOY_KEY
 ```
 
@@ -93,8 +79,7 @@ post fix it with `_PRIVATE` or `_PUBLIC`.
 
 # Special thanks
 
-- [wernerb](https://app.wercker.com/#wernerb) for fixing submodule support and
-  allowing the repository to be reused.
+- [wercker/step-heroku-deploy](https://github.com/wercker/step-heroku-deploy)
 
 # License
 
@@ -102,104 +87,6 @@ The MIT License (MIT)
 
 # Changelog
 
-## 3.0.1
+## 3.0.2
 
-- Fix sending randomly generating SSH keys to Heroku
-
-## 3.0.0
-
-- Refactor
-
-## 2.2.1
-
-- fixed: ssh: connect to host git.heroku.com port 22: Connection timed out
-
-## 2.2.0
-
-- Remove some changing of directories
-- Mark `source_dir` as deprecated. In a future version this will be replaced
-  with `cwd`, currently they however are not compatible.
-
-## 2.1.0
-
-- Check if API key is still valid.
-- Check if app exists on Heroku.
-- Suppress output of curl.
-
-## 2.0.2
-
-- fix: allow step to be run multiple times in a single deploy
-
-## 2.0.1
-
-- add install_toolbelt option
-
-## 2.0.0
-
-- Major refactor:
-- Only install toolbelt if needed (currently only required for running a
-  command).
-- Fix security issue, where the host key was ignored.
-- run.sh now uses functions.
-
-## 1.0.14
-
-- Add `keep-repository` parameter.
-
-## 1.0.11
-
-- Fix pushing submodules to heroku by flattening submodules if found.
-
-## 1.0.9
-
-- Make sure git-core is installed when heroku toolbelt.
-
-## 1.0.7
-
-* Fix bugs related to the `run` parameter.
-
-## 1.0.5
-
-* Update README, to clearify using a SSH Key pair.
-
-## 1.0.4
-
-* Fixed release that also works on ruby based boxes (always installs ruby 1.9.1)
-
-## 1.0.1
-
-* fix for apt-get bug: deb files are now included [broken]
-
-## 1.0.0
-
-* Bug fixes
-
-## 0.0.9-dev
-
-* Adds `run` option.
-
-## 0.0.8
-
-* Retry on failure.
-
-## 0.0.7
-
-* Only call `heroku keys:remove` if a ephemeral key was used
-  (wercker/step-heroku-deploy#2).
-* Update README.
-
-## 0.0.6
-
-* Fix wrong option check.
-
-## 0.0.4
-
-* Added validation to `key-name` option.
-
-## 0.0.3
-
-* Added `key-name` option.
-
-## 0.0.2
-
-* Initial release.
+- Fork wercker/step-heroku-deploy
--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@
 set -e;
 
 main() {
-    local exit_code_push=0;
-    local exit_code_run=0;
     local gitssh_path;
     local ssh_key_path;
 
@@ -22,7 +20,7 @@ main() {
     # Only test authentication if:
     # - A run command was specified, or
     # - No custom ssh key was specified
-    if [ -n "$WERCKER_HEROKU_DEPLOY_RUN" -o -z "$WERCKER_HEROKU_DEPLOY_KEY_NAME" ]; then
+    if [ -z "$WERCKER_HEROKU_DEPLOY_KEY_NAME" ]; then
         # verify user and key not empty
         if [ -z "$WERCKER_HEROKU_DEPLOY_USER" ]; then
             fail "user property is required"
@@ -42,61 +40,12 @@ main() {
         use_random_ssh_key "$ssh_key_path";
     fi
 
-    # Then check if the user wants to use the git repository or use the files in the source directory
-    if [ "$WERCKER_HEROKU_DEPLOY_KEEP_REPOSITORY" == "true" ]; then
-        use_current_git_directory "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR" "$WERCKER_GIT_BRANCH";
-    else
-        use_new_git_repository "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR";
-    fi
-
-    # Try to push the code
-    set +e;
-    push_code "$WERCKER_HEROKU_DEPLOY_APP_NAME";
-    exit_code_push=$?
-    set -e;
-
-    # Retry pushing the code, if the first push failed and retry was not disabled
-    if [ $exit_code_push -ne 0 ]; then
-        if [ "$WERCKER_HEROKU_DEPLOY_RETRY" == "false" ]; then
-            info "push failed, not going to retry";
-        else
-            info "push failed, retrying push in 5 seconds";
-            sleep 5;
-
-            set +e;
-            push_code "$WERCKER_HEROKU_DEPLOY_APP_NAME";
-            exit_code_push=$?
-            set -e;
-        fi
-    fi
-
-    if [ "$WERCKER_HEROKU_DEPLOY_INSTALL_TOOLBELT" == "true" -o -n "$WERCKER_HEROKU_DEPLOY_RUN" ]; then
-        install_toolbelt;
-    fi
-
-    # Run a command, if the push succeeded and the user supplied a run command
-    if [ -n "$WERCKER_HEROKU_DEPLOY_RUN" ]; then
-        if [ $exit_code_push -eq 0 ]; then
-            set +e;
-            execute_heroku_command "$WERCKER_HEROKU_DEPLOY_APP_NAME" "$WERCKER_HEROKU_DEPLOY_RUN";
-            exit_code_run=$?
-            set -e;
-        fi
-    fi
+    use_current_git_directory "$WERCKER_HEROKU_DEPLOY_SOURCE_DIR" "$WERCKER_GIT_BRANCH";
+    install_toolbelt;
 
     # Remove a auto generated key (assuming we generated a public key at ${ssh_key_path}.pub)
     if [ -z "$WERCKER_HEROKU_DEPLOY_KEY_NAME" ]; then
         remove_ssh_key "${ssh_key_path}.pub";
-    fi
-
-    if [ $exit_code_run -ne 0 ]; then
-        fail 'heroku run failed';
-    fi
-
-    if [ $exit_code_push -eq 0 ]; then
-        success 'deployment to heroku finished successfully';
-    else
-        fail 'git push to heroku failed';
     fi
 }
 
@@ -239,29 +188,6 @@ use_random_ssh_key() {
     add_ssh_key "${ssh_key_path}.pub";
 }
 
-push_code() {
-    local app_name="$1";
-
-    debug "starting heroku deployment with git push";
-    git push -f "git@heroku.com:$app_name.git" HEAD:master;
-    local exit_code_push=$?;
-
-    debug "git pushed exited with $exit_code_push";
-    return $exit_code_push;
-}
-
-execute_heroku_command() {
-    local app_name="$1";
-    local command="$2";
-
-    debug "starting heroku run $command";
-    heroku run "$command" --app "$app_name";
-    local exit_code_run=$?;
-
-    debug "heroku run exited with $exit_code_run";
-    return $exit_code_run;
-}
-
 add_ssh_key() {
     local public_key_path="$1";
 
@@ -308,33 +234,6 @@ use_current_git_directory() {
     git checkout "$branch"
 }
 
-use_new_git_repository() {
-    local working_directory="$1"
-
-    # If there is a git repository, remove it because
-    # we want to create a new git repository to push
-    # to heroku.
-    if [ -d "$working_directory/.git" ]; then
-        debug "found git repository in $working_directory"
-        warn "Removing git repository from $working_directory"
-        rm -rf "$working_directory/.git"
-
-        #submodules found are flattened
-        if [ -f "$working_directory/.gitmodules" ]; then
-            debug "found possible git submodule(s) usage"
-            while IFS= read -r -d '' file
-            do
-                rm -f "$file" && warn "Removed submodule $file"
-            done < <(find "$working_directory" -type f -name ".git" -print0)
-        fi
-    fi
-
-    # Create git repository and add all files.
-    # This repository will get pushed to heroku.
-    git init
-    git add .
-    git commit -m 'wercker deploy'
-}
 
 test_authentication() {
     local app_name="$1"
